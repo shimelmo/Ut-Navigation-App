12151bc36338f48d0f36552610adacea99170df5
Jacob <JacobVajcner@yahoo.com>
2026-02-19 16:45:22 -0500

    Proper Title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Jacob's Test Push</ThemedText>
+        <ThemedText type="title">UT Navigation App</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
